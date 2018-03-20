550333c5cfe9573f580c80bf113676ecb2bf588f
zyed <zyed.benmed@gmail.com>
2018-03-20 13:44:03 +0100

    add drawables

--- Eniso/app/src/main/java/com/example/zyed/eniso/MainActivity.java
+++ Eniso/app/src/main/java/com/example/zyed/eniso/MainActivity.java
@@ -1,13 +1,61 @@
 package com.example.zyed.eniso;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
 
 public class MainActivity extends AppCompatActivity {
+    LinearLayout l1, l2;
+    Button btnsub;
+    Animation uptodown, downtoup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //btnsub = (Button)findViewById(R.id.buttonsub);
+        l1 = (LinearLayout) findViewById(R.id.l1);
+        l2 = (LinearLayout) findViewById(R.id.l2);
+        uptodown = AnimationUtils.loadAnimation(this, R.anim.uptodown);
+        downtoup = AnimationUtils.loadAnimation(this, R.anim.downtoup);
+        l1.setAnimation(uptodown);
+        l2.setAnimation(downtoup);
+        Thread timer = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+
+                    Intent i = new Intent(getApplicationContext(), loginscreen.class);
+                    startActivity(i);
+                    finish();
+                    super.run();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+
+        };
+
+        timer.start();
+
     }
 }
+
+
+
+/*<Button
+
+        android:id="@+id/buttonsub"
+                android:layout_width="wrap_content"
+                android:layout_height="wrap_content"
+                android:layout_gravity="center"
+                android:background="@drawable/buttonstyle"
+                android:text="SUBSCRIBE NOW" />*/
