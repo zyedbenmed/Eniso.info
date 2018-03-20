b43dab330189e664a2bcdc728c8009d3dd6c8c51
zyed <zyed.benmed@gmail.com>
2018-03-20 15:55:59 +0100

    add drawables

--- Eniso/app/src/main/java/com/example/zyed/eniso/loginscreen.java
+++ Eniso/app/src/main/java/com/example/zyed/eniso/loginscreen.java
@@ -1,7 +1,11 @@
 package com.example.zyed.eniso;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class loginscreen extends AppCompatActivity {
 
@@ -9,5 +13,39 @@ public class loginscreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loginscreen);
+
+        Button b_more =(Button)findViewById(R.id.more);
+        Button b_join =(Button)findViewById(R.id.join);
+        Button b_login =(Button)findViewById(R.id.login);
+
+        final EditText ed_username=(EditText)findViewById(R.id.username);
+        final EditText ed_password=(EditText)findViewById(R.id.password);
+
+        b_more.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),more.class);
+                startActivity(i);
+
+            }
+        });
+
+        b_join.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),join.class);
+                startActivity(i);
+            }
+        });
+
+        b_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),login.class);
+                startActivity(i);
+            }
+        });
+
     }
+
 }
---
+++ Eniso/app/src/main/java/com/example/zyed/eniso/more.java
@@ -0,0 +1,26 @@
+package com.example.zyed.eniso;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class more extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_more);
+
+        final Button b_back =(Button)findViewById(R.id.back);
+
+        b_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),loginscreen.class);
+                startActivity(i);
+            }
+        });
+    }
+}
